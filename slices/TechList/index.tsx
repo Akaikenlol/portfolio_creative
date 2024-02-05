5dde44009643f26edf5561482f5a94b5ac6358be
Akaikenlol <keithken972@gmail.com>
2024-02-05 13:26:15 +0630

    style: Favicon chore: Text Animation Changes

--- slices/TechList/index.tsx
+++ slices/TechList/index.tsx
@@ -20,80 +20,80 @@ export type TechListProps = SliceComponentProps<Content.TechListSlice>;
  * Component for "TechList" Slices.
  */
 const TechList = ({ slice }: TechListProps): JSX.Element => {
-	const component = useRef(null);
+  const component = useRef(null);
 
-	useEffect(() => {
-		let ctx = gsap.context(() => {
-			const tl = gsap.timeline({
-				scrollTrigger: {
-					trigger: component.current,
-					start: "top bottom",
-					end: "bottom top",
-					scrub: 10,
-				},
-			});
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: component.current,
+          start: "top bottom",
+          end: "bottom top",
+          scrub: 10,
+        },
+      });
 
-			tl.fromTo(
-				".tech-row",
-				{
-					x: (index) => {
-						return index % 2 === 0
-							? gsap.utils.random(400, 200)
-							: gsap.utils.random(-400, -200);
-					},
-				},
-				{
-					x: (index) => {
-						return index % 2 === 0
-							? gsap.utils.random(-600, -400)
-							: gsap.utils.random(600, 400);
-					},
-					ease: "power3.inOut",
-				}
-			);
-		}, component);
+      tl.fromTo(
+        ".tech-row",
+        {
+          x: (index) => {
+            return index % 2 === 0
+              ? gsap.utils.random(600, 400)
+              : gsap.utils.random(-600, -400);
+          },
+        },
+        {
+          x: (index) => {
+            return index % 2 === 0
+              ? gsap.utils.random(-600, -400)
+              : gsap.utils.random(600, 400);
+          },
+          ease: "power3.inOut",
+        },
+      );
+    }, component);
 
-		return () => ctx.revert();
-	});
+    return () => ctx.revert();
+  });
 
-	return (
-		<section
-			data-slice-type={slice.slice_type}
-			data-slice-variation={slice.variation}
-			className="overflow-hidden"
-			ref={component}
-		>
-			<Bounded as="div">
-				<Heading size="xl" as="h2" className="mb-8">
-					{slice.primary.heading}
-				</Heading>
-			</Bounded>
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="overflow-hidden"
+      ref={component}
+    >
+      <Bounded as="div">
+        <Heading size="xl" as="h2" className="mb-8">
+          {slice.primary.heading}
+        </Heading>
+      </Bounded>
 
-			{slice.items.map(({ tech_color, tech_name }, index) => (
-				<div
-					key={index}
-					className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
-					aria-label={tech_name || undefined}
-				>
-					{Array.from({ length: 15 }, (_, index) => (
-						<React.Fragment key={index}>
-							<span
-								className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
-								style={{
-									color: index === 7 && tech_color ? tech_color : "inherit",
-								}}
-							>
-								{tech_name}
-							</span>
-							<span className="text-3xl">
-								<MdCircle />
-							</span>
-						</React.Fragment>
-					))}
-				</div>
-			))}
-		</section>
-	);
+      {slice.items.map(({ tech_color, tech_name }, index) => (
+        <div
+          key={index}
+          className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
+          aria-label={tech_name || undefined}
+        >
+          {Array.from({ length: 15 }, (_, index) => (
+            <React.Fragment key={index}>
+              <span
+                className="tech-item text-8xl font-extrabold uppercase tracking-tighter"
+                style={{
+                  color: index === 7 && tech_color ? tech_color : "inherit",
+                }}
+              >
+                {tech_name}
+              </span>
+              <span className="text-3xl">
+                <MdCircle />
+              </span>
+            </React.Fragment>
+          ))}
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default TechList;
